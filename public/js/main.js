89592b110cee6fa497d29c016133aac912d165c0
juan david marin <juaanmarin2007@gmail.com>
2025-01-19 21:25:32 -0500

    se agregaron vistas y se mdifico el menu

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,21 @@ menuToggle.addEventListener('click', () => {
     navigation.classList.toggle('active');
     menuToggle.textContent = navigation.classList.contains('active') ? '✖' : '☰';
 });
+
+// Selecciona los elementos del menú con submenú
+const submenuToggle = document.querySelector('.menu-item-with-submenu');
+
+// Agrega un evento para alternar el submenú
+submenuToggle.addEventListener('click', (event) => {
+     
+    // event.preventDefault(); // Previene la navegación si el clic no es en un enlace
+    submenuToggle.classList.toggle('active');
+
+    // if (event.target.classList.contains('menuitem')) {
+    //     console.log('holii');
+    //     // event.preventDefault(); // Solo para el menú principal
+    //     // Si el clic fue en un enlace, no bloquear la acción predeterminada
+    //     // return;
+    // }
+
+});
